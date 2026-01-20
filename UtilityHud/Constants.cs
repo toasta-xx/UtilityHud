5562aae38811121352d41eb451fa7d3e351148af
toasta.xx <nuhuh>
2026-01-20 16:25:49 -0600

    Add project files.

---
+++ UtilityHud/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace UtilityHud
+{
+    internal class Constants
+    {
+        public const string GUID = "net.toast.utilhud";
+        public const string NAME = "UtilityHUD";
+        public const string VERS = "1.0.0";
+    }
+}
